9cfb7281e45ab3ca83f40ed25d9396d936319599
Joey Park <61180930+joeyPark8@users.noreply.github.com>
2022-01-13 16:08:21 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import sys
 
-from main import app as main
+from app import create_app
 from flask_script import Manager
 from flask_twisted import Twisted
 from twisted.python import log
 
 if __name__ == '__main__':
-    app = main
+    app = create_app()
 
     twisted = Twisted(app)
     log.startLogging(sys.stdout)
